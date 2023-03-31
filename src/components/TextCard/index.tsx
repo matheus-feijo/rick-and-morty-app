34bafae0bf674f0dbf69067d8f73468982d6caa5
Matheus Feijó <matheus.feijo@sydy.com.br>
2023-03-31 14:49:33 -0400

    refactor responsividade com 400px

--- src/components/TextCard/index.tsx
+++ src/components/TextCard/index.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const TextCard = styled.h4`
   font-size: 18px;
 
-  @media (max-width: 350px) {
+  @media (max-width: 400px) {
     font-size: 14px;
     max-width: 100px;
   }
--- src/pages/DetailCharacter/styled.ts
+++ src/pages/DetailCharacter/styled.ts
@@ -4,7 +4,7 @@ export const ImgDetailCharacter = styled.img`
   border: 10px solid #ffff;
   border-radius: 8px;
 
-  @media (max-width: 350px) {
+  @media (max-width: 400px) {
     width: 250px;
   }
 `;
@@ -13,7 +13,7 @@ export const TextDetailCharacter = styled.p`
   font-size: 28px;
   color: #ffff;
 
-  @media (max-width: 350px) {
+  @media (max-width: 400px) {
     font-size: 21px;
   }
 `;
